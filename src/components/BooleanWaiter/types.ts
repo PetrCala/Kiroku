b5aa4232fef0063f4de373eb1bf97d2bf12fa420
PetrCala <cala.p@seznam.cz>
2025-01-20 11:06:23 +0100

    Remove the BooleanWaiter component

--- src/components/BooleanWaiter/types.ts
+++
@@ -1,8 +0,0 @@
-type BooleanWaiterProps = {
-  value: boolean;
-  timeout?: number;
-  onTimeout: () => void; // Callback for when the timeout is exceeded
-  onResolved: () => void; // Callback for when the boolean becomes true
-};
-
-export type {BooleanWaiterProps};
