a2d4306ac314bdb03cc1886082c0e7258cd11f40
PetrCala <cala.p@seznam.cz>
2025-01-20 11:05:48 +0100

    Remove the AsyncResponsiveComponent

--- src/components/AsyncResponsiveComponent/types.ts
+++
@@ -1,9 +0,0 @@
-type AsyncResponsiveComponentProps = {
-  trigger: any; // This could be more specifically typed depending on the use case
-  asyncOperation: (
-    triggerRef: React.MutableRefObject<any>,
-    checkIsActive: () => boolean,
-  ) => Promise<void>;
-};
-
-export type {AsyncResponsiveComponentProps};
