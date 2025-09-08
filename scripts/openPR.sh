8f0aa37a3a46d4059582d2d1d7c1739d1b929647
PetrCala <cala.p@seznam.cz>
2025-09-08 13:28:05 +0200

    ci: add scripts for better PR handling

---
+++ scripts/openPR.sh
@@ -0,0 +1,62 @@
+
+#!/bin/bash
+set -e
+
+SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
+source "$SCRIPTS_DIR/shellUtils.sh"
+
+REVIEWER="KirokuAdmin"
+
+usage() {
+  echo "Usage: $0  [-r | --release] --semver <SEMVER_LEVEL>"
+  exit 1
+}
+
+READY_TO_BUILD=""
+SEMVER_LEVEL="patch"
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --semver)
+    SEMVER_LEVEL="$2"
+    shift 2
+    ;;
+  -r | --release)
+    READY_TO_BUILD="true"
+    shift
+    ;;
+  *)
+    usage
+    ;;
+  esac
+done
+
+if [[ $(git status --porcelain) ]]; then
+  error "There are unsaved changes. Please commit or stash your changes before running this script."
+  exit 1
+fi
+
+RELEASE_BRANCH="master"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$CURRENT_BRANCH" == "$RELEASE_BRANCH" ]]; then
+  error "You are already on the $RELEASE_BRANCH branch. Please switch to another branch before running this script."
+  exit 1
+fi
+
+info "Opening PR from branch $CURRENT_BRANCH to branch $RELEASE_BRANCH..."
+
+RELEASE_VERSION=$(jq -r .version 'package.json')
+SEMVER_LEVEL=$(echo "$SEMVER_LEVEL" | tr '[:upper:]' '[:lower:]') # to lowercase
+
+gh pr create \
+  --title "New version release" \
+  --body "Automated release PR" \
+  --base "$RELEASE_BRANCH" \
+  --head "$CURRENT_BRANCH" \
+  --reviewer "$REVIEWER" \
+  ${READY_TO_BUILD:+--label release} \
+  --label "v-$SEMVER_LEVEL"
+
+success "Release PR opened!"
