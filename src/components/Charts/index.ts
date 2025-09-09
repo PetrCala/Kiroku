350ac86f4a623847a7e31b5e79cce6331f744ad1
PetrCala <cala.p@seznam.cz>
2025-09-09 12:33:14 +0200

    feat: split the stacked bars chart element

--- src/components/Charts/index.ts
+++ src/components/Charts/index.ts
@@ -1,5 +1,6 @@
 export {default as KpiTile} from './KpiTile';
 export {default as BarsWeekly} from './BarsWeekly';
 export {default as LineTrend} from './LineTrend';
+export {default as StackedBars} from './StackedBars';
 export {default as StackedBarsByType} from './StackedBarsByType';
 export {default as HeatmapCalendar} from './HeatmapCalendar';
